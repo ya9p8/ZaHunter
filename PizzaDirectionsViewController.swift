fde787a6db2ed48c7fd885aa646341dcacb483cd
Yemi Ajibola <ya9p8@mst.edu>
2016-02-03 14:12:43 -0600

    Preliminary storyboard mapping.

---
+++ PizzaDirectionsViewController.swift
@@ -0,0 +1,41 @@
+//
+//  PizzaDirectionsViewController.swift
+//  ZaHunter
+//
+//  Created by Yemi Ajibola on 2/3/16.
+//  Copyright © 2016 Yemi Ajibola. All rights reserved.
+//
+
+import UIKit
+import CoreLocation
+import MapKit
+
+class PizzaDirectionsViewController: UIViewController
+{
+
+    @IBOutlet weak var pizzeriaTableView: UITableView!
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning()
+    {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
